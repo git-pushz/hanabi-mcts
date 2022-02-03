f7e688b2ce7730c9e7befa9b6812996398051f36
Riccardo Sepe <seric814@gmail.com>
2022-02-03 15:46:32 +0100

    Minor changes

--- project/hanabi/libs/game_state.py
+++ project/hanabi/libs/game_state.py
@@ -374,7 +374,7 @@ class MCTSState(GameState):
         If the game isn't ended, it returns False, None
         """
         if self.errors == MAX_ERRORS:
-            return True, sum(self.board) // 3
+            return True, sum(self.board) // 2
             # return True, 0
         # if self.board == self.trash.maxima:
         if np.all(self.board == 5):
